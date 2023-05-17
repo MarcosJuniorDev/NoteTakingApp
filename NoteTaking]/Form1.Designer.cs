92a2e759f43a3803dfbc7016ac35e9628d45465a
Marcos Junior Oliveira <marcosjoliveiradev@gmail.com>
2023-05-17 17:49:59 -0300

    programa pronto

--- NoteTaking]/Form1.Designer.cs
+++ NoteTaking]/Form1.Designer.cs
@@ -28,61 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.titleBox = new System.Windows.Forms.Label();
-            this.noteBox = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.titleBox = new System.Windows.Forms.TextBox();
+            this.noteBox = new System.Windows.Forms.TextBox();
+            this.title = new System.Windows.Forms.Label();
+            this.note = new System.Windows.Forms.Label();
+            this.previusNote = new System.Windows.Forms.DataGridView();
             this.saveButton = new System.Windows.Forms.Button();
             this.loadButton = new System.Windows.Forms.Button();
             this.deleteButton = new System.Windows.Forms.Button();
             this.newButton = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.previusNote)).BeginInit();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(411, 33);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(377, 23);
-            this.textBox1.TabIndex = 0;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(411, 94);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(377, 344);
-            this.textBox2.TabIndex = 1;
             // 
             // titleBox
             // 
-            this.titleBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.titleBox.Location = new System.Drawing.Point(471, 7);
+            this.titleBox.Location = new System.Drawing.Point(411, 33);
             this.titleBox.Name = "titleBox";
-            this.titleBox.Size = new System.Drawing.Size(317, 23);
-            this.titleBox.TabIndex = 2;
-            this.titleBox.Text = "Title";
-            this.titleBox.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.titleBox.Size = new System.Drawing.Size(377, 23);
+            this.titleBox.TabIndex = 0;
             // 
             // noteBox
             // 
-            this.noteBox.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.noteBox.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.noteBox.Location = new System.Drawing.Point(471, 59);
+            this.noteBox.Location = new System.Drawing.Point(411, 94);
+            this.noteBox.Multiline = true;
             this.noteBox.Name = "noteBox";
-            this.noteBox.Size = new System.Drawing.Size(317, 23);
-            this.noteBox.TabIndex = 3;
-            this.noteBox.Text = "Note:";
+            this.noteBox.Size = new System.Drawing.Size(377, 344);
+            this.noteBox.TabIndex = 1;
             // 
-            // dataGridView1
+            // title
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(393, 347);
-            this.dataGridView1.TabIndex = 4;
+            this.title.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.title.Location = new System.Drawing.Point(471, 7);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(317, 23);
+            this.title.TabIndex = 2;
+            this.title.Text = "Title";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // note
+            // 
+            this.note.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.note.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.note.Location = new System.Drawing.Point(459, 59);
+            this.note.Name = "note";
+            this.note.Size = new System.Drawing.Size(317, 23);
+            this.note.TabIndex = 3;
+            this.note.Text = "Note:";
+            // 
+            // previusNote
+            // 
+            this.previusNote.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.previusNote.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.previusNote.Location = new System.Drawing.Point(12, 12);
+            this.previusNote.Name = "previusNote";
+            this.previusNote.RowTemplate.Height = 25;
+            this.previusNote.Size = new System.Drawing.Size(393, 347);
+            this.previusNote.TabIndex = 4;
+            this.previusNote.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.previusNote_CellDoubleClick);
             // 
             // saveButton
             // 
@@ -138,15 +140,15 @@
             this.Controls.Add(this.deleteButton);
             this.Controls.Add(this.loadButton);
             this.Controls.Add(this.saveButton);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.previusNote);
+            this.Controls.Add(this.note);
+            this.Controls.Add(this.title);
             this.Controls.Add(this.noteBox);
             this.Controls.Add(this.titleBox);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
             this.Name = "noteTaker";
             this.Text = "noteTaker";
             this.Load += new System.EventHandler(this.noteTaker_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.previusNote)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -154,11 +156,11 @@
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label titleBox;
-        private Label noteBox;
-        private DataGridView dataGridView1;
+        private TextBox titleBox;
+        private TextBox noteBox;
+        private Label title;
+        private Label note;
+        private DataGridView previusNote;
         private Button saveButton;
         private Button loadButton;
         private Button deleteButton;
--- NoteTaking]/Form1.cs
+++ NoteTaking]/Form1.cs
@@ -1,7 +1,14 @@
+using System.Data;
+
 namespace NoteTaking_
 {
     public partial class noteTaker : Form
     {
+        public DataTable Notes { get; set; } = new DataTable();
+        public bool Editing { get; set; } = false;
+
+
+
         public noteTaker()
         {
             InitializeComponent();
@@ -9,28 +16,59 @@ namespace NoteTaking_
 
         private void noteTaker_Load(object sender, EventArgs e)
         {
+            Notes.Columns.Add("Title");
+            Notes.Columns.Add("Note");
 
+            previusNote.DataSource = Notes;
 
         }
 
         private void saveButton_Click(object sender, EventArgs e)
         {
-
+            if (Editing)
+            {
+                Notes.Rows[previusNote.CurrentCell.RowIndex]["Title"] = titleBox.Text;
+                Notes.Rows[previusNote.CurrentCell.RowIndex]["Note"] = noteBox.Text;
+            }
+            else
+            {
+                Notes.Rows.Add(titleBox.Text, noteBox.Text);
+            }
+            titleBox.Text = "";
+            noteBox.Text = "";
+            Editing = false;
         }
 
         private void loadButton_Click(object sender, EventArgs e)
         {
-
+            titleBox.Text = Notes.Rows[previusNote.CurrentCell.RowIndex].ItemArray[0].ToString();
+            noteBox.Text = Notes.Rows[previusNote.CurrentCell.RowIndex].ItemArray[1].ToString();
+            Editing = true;
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
-
+            try
+            {
+                Notes.Rows[previusNote.CurrentCell.RowIndex].Delete();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Nota não valida {ex.Message}");
+            }
         }
 
         private void newButton_Click(object sender, EventArgs e)
         {
+            titleBox.Text = "";
+            noteBox.Text = "";
+        }
 
+        private void previusNote_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            title.Text = Notes.Rows[previusNote.CurrentCell.RowIndex].ItemArray[0].ToString();
+            note.Text = Notes.Rows[previusNote.CurrentCell.RowIndex].ItemArray[1].ToString();
+            Editing = true;
         }
     }
 }
